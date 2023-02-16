f437112becec878b68500161ced489d3328400dd
Chillwave Strong <chillwav@ge-a4.kzn.21-school.ru>
2023-02-16 16:25:31 +0300

    ex #1 is done

--- src/1/main.go
+++ src/1/main.go
@@ -1,1 +1,38 @@
-package _
+package main
+
+import "fmt"
+
+type Human struct {
+	Name string
+	Age  int
+	test string
+}
+
+type Action struct {
+	Human
+	test string
+}
+
+func (h *Human) Hi() {
+	fmt.Println("Hello from Human")
+}
+
+func (h *Human) Bye() {
+	fmt.Printf("Bye from %s\n", h.Name)
+}
+
+func (a *Action) Hi() {
+	fmt.Println("Hello from Action")
+}
+
+func main() {
+	a := Action{Human: Human{Name: "Dima", Age: 22, test: "test-dima"}}
+	b := Action{Human: Human{Name: "Vadim", Age: 15, test: "test-vadim"}, test: "test-updated"}
+	fmt.Println(a.Name, b.Name)
+	fmt.Println(a.Age, b.Age)
+	fmt.Println(a.test, b.test)
+	a.Hi()
+	b.Hi()
+	a.Bye()
+	b.Bye()
+}
