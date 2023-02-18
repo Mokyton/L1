c980d20dc8adf51ed8375ee2f68da145c4cd0e16
Chillwave Strong <chillwav@ge-a4.kzn.21-school.ru>
2023-02-18 20:13:07 +0300

    ex #17 is done

---
+++ src/17/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
+
+func main() {
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	index, _ := binSearch(data, 10)
+	fmt.Println(index)
+}
+
+func binSearch(data []int, val int) (int, error) {
+	if !sort.SliceIsSorted(data, func(i, j int) bool {
+		return data[i] < data[j]
+	}) {
+		return -1, errors.New("Error: unsorted slice ")
+	}
+
+	left := 0
+	right := len(data) - 1
+	for left <= right {
+		mid := (right + left) / 2
+		if val == data[mid] {
+			return mid, nil
+		}
+		if val > data[mid] {
+			left = mid + 1
+		}
+		if val < data[mid] {
+			right = mid - 1
+		}
+	}
+	return -1, errors.New("This elements doesn't exist ")
+}
